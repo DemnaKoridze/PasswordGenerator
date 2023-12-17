f109c96b5184edb85c18639dd29900bb1ac35bd9
Demna Koridze <demnaqoridze@gmail.com>
2023-12-18 00:05:19 +0400

    Initial Commit

---
+++ PasswordGenerator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PasswordGenerator
+//
+//  Created by demnakoridze on 18.12.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
