e17d462f36a9f64e44d8a0b0bfdcd0fe4d3f8e0b
Demna Koridze <demnaqoridze@gmail.com>
2023-12-19 16:09:51 +0400

    This code will generate a password

--- PasswordGenerator/main.cpp
+++ PasswordGenerator/main.cpp
@@ -1,14 +1,43 @@
-//
-//  main.cpp
-//  PasswordGenerator
-//
-//  Created by demnakoridze on 18.12.23.
-//
-
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+string passwordGenerator(int length){
+    string password="";
+    const string symbols="qwertyuiopasdfghjklzxcvbnm";
+    const int symbolsLength=symbols.length();
+    srand(time(0));
+    for(int i=0; i<length; i++){
+        
+        int randomizer=rand()%symbolsLength;
+        
+        password+= symbols[randomizer];
+        
+    }return password;
+    
+}
+
+ 
+int main() {
+    int userInput;
+    cout<<"Hello,i can generate a password.How many symbols it should have ? "<<endl;
+    
+    cin >> userInput;
+    
+    if(userInput>8&&userInput<30){
+        
+        cout<< "Your password is :  "<<passwordGenerator(userInput)<<endl;
+        
+    }else if(userInput<=8||userInput>29){
+        
+        cout<< "Your Input is not valid, it must be between 8 and 30"<<endl;
+        
+    }
+    
+    
+    
+    
     return 0;
 }
